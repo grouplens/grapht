3ce2ba5c779bdc182a6e0ae45882feb25c712385
Michael Ekstrand <ekstrand@cs.umn.edu>
2012-08-08 12:52:34 -0500

    Mark MemoizingProvider as thread-safe

--- src/main/java/org/grouplens/grapht/util/MemoizingProvider.java
+++ src/main/java/org/grouplens/grapht/util/MemoizingProvider.java
@@ -18,31 +18,33 @@
  */
 package org.grouplens.grapht.util;
 
+import javax.annotation.concurrent.ThreadSafe;
 import javax.inject.Provider;
 
 /**
  * MemoizingProvider is a Provider that enforces memoization or caching on
  * another Provider that it wraps.
- * 
- * @author Michael Ludwig <mludwig@cs.umn.edu>
+ *
  * @param <T>
+ * @author Michael Ludwig <mludwig@cs.umn.edu>
  */
+@ThreadSafe
 public class MemoizingProvider<T> implements Provider<T> {
     private final Provider<T> wrapped;
-    
+
     // We track a boolean because this supports providing null instances, in
     // which case we can't just check against null to see if we've already
     // queried the base provider
     private volatile T cached;
     private volatile boolean invoked;
-    
+
     public MemoizingProvider(Provider<T> provider) {
         Preconditions.notNull("provider", provider);
         wrapped = provider;
         cached = null;
         invoked = false;
     }
-    
+
     @Override
     public T get() {
         if (!invoked) {
