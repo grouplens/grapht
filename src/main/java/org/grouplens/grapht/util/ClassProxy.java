c8aee1e0b055c23223d658501b9f64e39b370239
Michael Ekstrand <ekstrand@cs.umn.edu>
2013-04-02 16:02:49 -0500

    Re-implement class checksumming (refs #54)

--- src/main/java/org/grouplens/grapht/util/ClassProxy.java
+++ src/main/java/org/grouplens/grapht/util/ClassProxy.java
@@ -19,11 +19,22 @@
 package org.grouplens.grapht.util;
 
 import org.apache.commons.lang3.ClassUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
 import java.io.Serializable;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A serialization proxy for class instances.  This serializable class encapsulates a simple
@@ -41,11 +52,13 @@ public final class ClassProxy implements Serializable {
     private static final long serialVersionUID = 1;
 
     private final String className;
+    private final byte[] checksum;
     @Nullable
     private transient volatile Class<?> theClass;
 
-    private ClassProxy(String name) {
+    private ClassProxy(String name, byte[] cksum) {
         className = name;
+        checksum = cksum;
     }
 
     /**
@@ -96,8 +109,53 @@ public final class ClassProxy implements Serializable {
      * @return The class proxy.
      */
     public static ClassProxy forClass(Class<?> cls) {
-        ClassProxy proxy = new ClassProxy(cls.getName());
+        ClassProxy proxy = new ClassProxy(cls.getName(), checksumClass(cls));
         proxy.theClass = cls;
         return proxy;
+    }
+
+    private static final Charset UTF8 = Charset.forName("UTF-8");
+
+    /**
+     * Compute a checksum for a class. These checksums are used to see if a class has changed
+     * its definition since being serialized.
+     *
+     * @param type The class to checksum.
+     * @return The class's checksum.
+     */
+    private static byte[] checksumClass(Class<?> type) {
+        // we compute a big hash of all the members of the class, and its superclasses.
+
+        List<String> members = new ArrayList<String>();
+        for (Constructor<?> c: type.getDeclaredConstructors()) {
+            members.add(String.format("%s(%s)", c.getName(),
+                                      StringUtils.join(c.getParameterTypes(), ", ")));
+        }
+        for (Method m: type.getDeclaredMethods()) {
+            members.add(String.format("%s(%s): %s", m.getName(),
+                                      StringUtils.join(m.getParameterTypes(), ", "),
+                                      m.getReturnType()));
+        }
+        for (Field f: type.getDeclaredFields()) {
+            members.add(f.getName() + ":" + f.getType().getName());
+        }
+
+        Collections.sort(members);
+
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("JVM does not support MD5");
+        }
+        Class<?> sup = type.getSuperclass();
+        if (sup != null) {
+            digest.update(checksumClass(sup));
+        }
+        for (String mem: members) {
+            digest.update(mem.getBytes(UTF8));
+        }
+
+        return digest.digest();
     }
 }
