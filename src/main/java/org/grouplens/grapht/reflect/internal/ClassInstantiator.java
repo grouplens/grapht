6478c540c7db061cc29605ab5ab074f9e3de99d8
Imran <m_k136@txstate.edu>
2015-06-14 02:24:08 -0500

    comments corrects

--- src/main/java/org/grouplens/grapht/reflect/internal/ClassInstantiator.java
+++ src/main/java/org/grouplens/grapht/reflect/internal/ClassInstantiator.java
@@ -1,11 +1,17 @@
 /*
  * Grapht, an open source dependency injector.
- * Copyright 2010-2012 Regents of the University of Minnesota and contributors
+ * Copyright 2014-2015 various contributors (see CONTRIBUTORS.txt)
+ * Copyright 2010-2014 Regents of the University of Minnesota
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1 of the
  * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more.
+ * details.
  *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 51
@@ -62,7 +68,6 @@ public class ClassInstantiator implements Instantiator {
         Preconditions.notNull("desires", desires);
         Preconditions.notNull("providers", providers);
         Preconditions.notNull("injectionContainer", injectionContainer);
-
 
         this.type = type;
         this.desires = desires;
