330adca8d855ce0ba2104a7b557024b15eab2266
Michael Ekstrand <ekstrand@txstate.edu>
2015-11-03 17:38:11 -0600

    only Sonar on JDK 8

--- etc/maybe-sonar.sh
+++ etc/maybe-sonar.sh
@@ -3,5 +3,6 @@
 [ "$TRAVIS_REPO_SLUG" == grouplens/grapht ] || exit 0
 [ "$TRAVIS_BRANCH" == master ] || exit 0
 [ -z "$TRAVIS_PULL_REQUEST" ] || exit 0
+[ "$TRAVIS_JDK_VERSION" = oraclejdk8 ] || exit 0
 
 exec mvn --batch-mode sonar:sonar
