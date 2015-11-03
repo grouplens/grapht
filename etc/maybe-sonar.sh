b39bd56828b23ee34cae22f630b44e0f374b3054
Michael Ekstrand <ekstrand@txstate.edu>
2015-11-03 17:42:58 -0600

    fix syntax in maybe-sonar.sh

--- etc/maybe-sonar.sh
+++ etc/maybe-sonar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-[ "$TRAVIS_REPO_SLUG" == grouplens/grapht ] || exit 0
-[ "$TRAVIS_BRANCH" == master ] || exit 0
+[ "$TRAVIS_REPO_SLUG" = grouplens/grapht ] || exit 0
+[ "$TRAVIS_BRANCH" = master ] || exit 0
 [ -z "$TRAVIS_PULL_REQUEST" ] || exit 0
 [ "$TRAVIS_JDK_VERSION" = oraclejdk8 ] || exit 0
 
